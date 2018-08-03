dcfe18e1ac583d7985f8a59d940d80d7121335bc
TheDevXen <thedevxen@gmail.com>
2018-08-03 16:54:21 +0530

    added computer eassy and trained

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 process.env = {
-    BOT_TOKEN: '665856759:AAGDLP9ux5AgFScbdhMAQwOLxM6VWajExEk',
+    BOT_TOKEN: '665856759:AAGVJ66-CTJjbTQs1yfeGO0X7B7LWsL6vus',
     BOT_USERNAME: 'CynaideBot',
     OWNERS: [
         400674061, // iamxen
--- index.js
+++ index.js
@@ -16,11 +16,7 @@ const bot = new telegraf(process.env.BOT_TOKEN, {
 bot.telegram.getMe().then((self) => {
 	bot.self = self;
 });
-
-
-
 bot.startPolling();
-
 const config = {
     inputSize: 50,
     inputRange: 20,
@@ -29,7 +25,11 @@ const config = {
     learningRate: 0.01,
     decayRate: 0.999,
 }
-
+bot.command('start', async(ctx) => {
+	console.log('test')
+	ctx.reply("Sorry, I am still learning.");
+})
+// bot.telegram.sendMessage( -1001193338541, 'Hi')
 const brain = require('brain.js');
 const fs = require('fs');
 const net = new brain.recurrent.LSTM(config);
@@ -40,28 +40,23 @@ var array = [];
 fs.readFile('learned.json', async (err, data) => {
 	if(err) return console.log(err)
 	if(data.toString() === '') {
-		bot.command('start', async(ctx) => {
-			console.log('test')
-			ctx.reply("Sorry, I am still learning.");
-		})
-		fs.readFile('eassy.txt', async (err, data) => {
-			if(!err){
-				console.log("Training...");
-				data = data.toString();
-				array = data.split('.');
-				console.log("Data Length: " + array.length)
-				train();
-			}
-		}) 
+		train();
 	} else {
 		console.log("Network already trained...");
-		boot();
+		fs.readFile('learned.json', (err, data) => {
+			data = data.toString();
+			net.fromJSON(JSON.parse(data));
+			boot();
+		})
 	}
 });
-
-const train = async () => {
-	net.train(array, {			                            // Defaults values --> expected validation
-	      iterations: 20000,    // the maximum times to iterate the training data --> number greater than 0
+ 
+const train = async (data) => {
+   	net.train([
+	  { input: 'I feel great about the world!', output: 'happy' },
+	  { input: 'The world is a terrible place!', output: 'sad' },
+	], {			                            // Defaults values --> expected validation
+	      iterations: 1000,    // the maximum times to iterate the training data --> number greater than 0
 	      errorThresh: 0.1,   // the acceptable error percentage from training data --> number between 0 and 1
 	      log: true,           // true to use console.log, when a function is supplied it is used --> Either true or a function
 	      logPeriod: 10,        // iterations between logging out --> number greater than 0
@@ -71,24 +66,35 @@ const train = async () => {
 	      callbackPeriod: 10,   // the number of iterations through the training data between callback calls --> number greater than 0
 	      timeout: Infinity     // the max number of milliseconds to train for --> number greater than 0
 	});
+
 	fs.writeFile('learned.json', JSON.stringify(net.toJSON()), (err) => {
 	  if(!err) console.log("File Written");
-
-
 	});
-	console.log(`Trained in ${ (new Date() - date ) / 10} s`);
+	console.log(`Trained in ${ (new Date() - date ) / 1000} s`);
 
 	boot();
 }
-
 const boot = async () => {
 	bot.on('message', async(ctx) => {
 		const text = ctx.message.text;
+		console.log("Running: "+text)
 		if (ctx.message.reply_to_message) {
-            if (!text.match(/^[a-zA-Z0-9]/i)) return;
+            if (typeof text !== 'undefined' && !text.match(/^[a-zA-Z0-9]/i)) return;
             if (ctx.message.reply_to_message.from.id === bot.self.id) {
-            	ctx.reply(net.run())
+            	var res;
+            	if(typeof text === 'undefined') return;
+            	try {
+            		res = net.run(text);
+            		ctx.reply(res, {parse_mode: null})
+            	} catch(e) {
+            		console.log(e)
+            		ctx.reply("Sorry, I dont know that.")
+            	}
+            	console.log(res)
         	}
         }
 	});
+	bot.command('start', async(ctx) => {
+		ctx.reply(`Hi, ${ctx.from.first_name}. I am up and running`);
+	})
 }
