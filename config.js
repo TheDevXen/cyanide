33b75e6346b5b04f7a024aab35dcb41a10a579b7
TheDevXen <thedevxen@gmail.com>
2018-08-03 07:54:28 +0530

    fixed and added bot reply when network trained

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 process.env = {
-    BOT_TOKEN: '',
-    BOT_USERNAME: '',
+    BOT_TOKEN: '665856759:AAGDLP9ux5AgFScbdhMAQwOLxM6VWajExEk',
+    BOT_USERNAME: 'CynaideBot',
     OWNERS: [
         400674061, // iamxen
     ]
